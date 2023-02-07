bb5c3e5f6b64bc401b05988fa7733a1df433d7b3
khalidmesbah <swekhalidmesbah6@gmail.com>
2023-02-07 17:07:53 +0200

    up

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/", routes);
 
 connectToDatabase()
   .then(() => {
+    app.use("/", routes);
+
     app.listen(process.env.PORT, () => {
       console.log(
         `the server is running on http://localhost:${process.env.PORT}`
